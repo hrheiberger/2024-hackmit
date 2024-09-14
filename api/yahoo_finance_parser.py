a223dd0e6e35a32352ff6cb26385967620ff9861
hrheiberger <henryrh@mit.edu>
2024-09-14 16:03:59 -0400

    Return only close

--- api/yahoo_finance_parser.py
+++ api/yahoo_finance_parser.py
@@ -6,8 +6,7 @@ def get_close_history(ticker):
     start = pandas.Timestamp('2024-09-09 00:00:00-0400', tz='America/New_York')
     history = company.history(period="max", interval="1wk", start=start)
 
-    closes = [(index, row.values[0]) for index, row in history[["Close"]].iterrows()]
+    closes = [(row.values[0]) for index, row in history[["Close"]].iterrows()]
     return closes[::-1]
 
 closes = get_close_history("YEXT")
-print(closes)
