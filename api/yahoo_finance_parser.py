b7b2df42513cb61e2b338c438bbc97da7eef6a8f
hrheiberger <henryrh@mit.edu>
2024-09-14 15:06:26 -0400

    Add get_close_history function

---
+++ api/yahoo_finance_parser.py
@@ -0,0 +1,10 @@
+import yfinance as yf 
+
+def get_close_history(ticker):
+    company = yf.Ticker(ticker)
+    history = company.history(period="max", interval="1wk")
+
+    closes = [row.values[0] for index, row in history[["Close"]].iterrows()]
+    return closes[::-1]
+
+print(get_close_history("COST"))
