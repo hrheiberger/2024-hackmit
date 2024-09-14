49688c211389e84fdf4306f61f2af317a80c688d
hrheiberger <henryrh@mit.edu>
2024-09-14 16:03:09 -0400

    Make get close history more accurate

--- research/yahoo_finance_parser.py
+++ research/yahoo_finance_parser.py
@@ -1,10 +1,13 @@
 import yfinance as yf 
+import pandas
 
 def get_close_history(ticker):
     company = yf.Ticker(ticker)
-    history = company.history(period="max", interval="1wk")
+    start = pandas.Timestamp('2024-09-09 00:00:00-0400', tz='America/New_York')
+    history = company.history(period="max", interval="1wk", start=start)
 
-    closes = [row.values[0] for index, row in history[["Close"]].iterrows()]
+    closes = [(index, row.values[0]) for index, row in history[["Close"]].iterrows()]
     return closes[::-1]
 
-print(get_close_history("COST"))
+closes = get_close_history("YEXT")
+print(closes)
