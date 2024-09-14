c145f66806f05698343c021022d30d12ba66176c
hrheiberger <henryrh@mit.edu>
2024-09-14 17:44:31 -0400

    Add get industry and get sector

--- research/yahoo_finance_parser.py
+++ research/yahoo_finance_parser.py
@@ -1,6 +1,17 @@
 import yfinance as yf 
 import pandas
 
+company = yf.Ticker("NVDA")
+print(company.info)
+
+def get_industry(ticker):
+    company = yf.Ticker(ticker)
+    return company.info["industryKey"]
+
+def get_sector(ticker):
+    company = yf.Ticker(ticker)
+    return company.info["sectorKey"]
+
 def get_close_history(ticker):
     company = yf.Ticker(ticker)
     start = pandas.Timestamp('2024-09-09 00:00:00-0400', tz='America/New_York')
@@ -9,4 +20,4 @@ def get_close_history(ticker):
     closes = [(row.values[0]) for index, row in history[["Close"]].iterrows()]
     return closes[::-1]
 
-closes = get_close_history("YEXT")
+closes = get_close_history("NVDA")
