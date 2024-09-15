99715bb00333c5ad455c7cb31b566bf68ac2b10b
hrheiberger <henryrh@mit.edu>
2024-09-15 00:40:14 -0400

    Call Convex action in App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,12 +6,18 @@ import {
   useMutation,
   useQuery,
 } from "convex/react";
+import { useAction } from "convex/react";
 import { api } from "../convex/_generated/api";
 import { NavComponent } from "./layout/nav";
 import { TypeAnimation } from 'react-type-animation';
 import { HomeComponent } from "./layout/home";
 
 export default function App() {
+  const performMyAction = useAction(api.YahooFinanceParser.getCompanyData);
+  console.log(performMyAction().then((data) => data.closes));
+
+
+
   return (
     <>
       <NavComponent />
